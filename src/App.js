7806cef5243c0950bb37757447d6d2d316a15234
Garrett <garrettleegrahamanderson@gmail.com>
2023-11-04 13:49:47 -0400

    created a services component for the provided services and added component to the app component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import project5 from "./assets/images/project5.jpeg";
 import project6 from "./assets/images/project6.jpeg";
 import "./App.css";
 import "./assets/styles/HamburgerMenu.css";
+import Services from "./components/Services";
 
 function App() {
   const scrollToTop = () => {
@@ -19,42 +20,7 @@ function App() {
     <div className="App">
       <Header />
       <main>
-        <section className="Services">
-          <ul>
-            <li className="Service Service-consultation" id="consultation">
-              <div className="Service-details">
-                <h3>Consultation</h3>
-                <p>
-                  From repairs and renovations to general maintainance, we can
-                  cover all aspects of your aquatic environment needs. With over
-                  30 years of experience, Martin Simmermacher has extensive
-                  knowledge to answer any question that may arise.
-                </p>
-              </div>
-            </li>
-            <li className="Service Service-restoration" id="restoration">
-              <div className="Service-details">
-                <h3>Restoration</h3>
-                <p>
-                  Make your pool look like new with an over haul of your
-                  existing aquatic environment. Proceed with confidence knowing
-                  that your needs and desires will be met by an expert.
-                </p>
-              </div>
-            </li>
-            <li className="Service Service-maintainance" id="maintainance">
-              <div className="Service-details">
-                <h3>Maintainance</h3>
-                <p>
-                  Rest assured that your aquatic enviorments will be maintained
-                  with utmost care by our trained and experienced technicians.
-                  There is no condition that cannot be improved in order to
-                  bring your pool back to life and keep it pristine.
-                </p>
-              </div>
-            </li>
-          </ul>
-        </section>
+        <Services />
         <section className="Photo-gallery" id="gallery">
           <section className="Photos">
             <img src={project1} className="Project-photo" alt="project-photo" />
